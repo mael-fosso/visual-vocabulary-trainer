b725664dfc04b7668a72e7b046079d074d250641
Mael Fosso <69007418+mael-fosso@users.noreply.github.com>
2020-09-08 02:58:23 +0200

    Update serviceworker.js

--- src/vocgui/static/serviceworker.js
+++ src/vocgui/static/serviceworker.js
@@ -1,6 +1,6 @@
 // 'use strict'
 importScripts('sw-toolbox.js')
-toolbox.precache(['index.html', 'src/css/style.min.css'])
+toolbox.precache(['src/vocgui/templates/index.html', 'src/css/style.min.css'])
 // toolbox.router.get('/images/*', toolbox.cacheFirst)
 toolbox.router.get('/*', toolbox.networkFirst, {
 networkTimeoutSeconds: 5})
